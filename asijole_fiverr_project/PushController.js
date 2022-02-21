3232845f36bdbdb6ccb505b473b41abd9e998361
Akash <akashrobo2@gmail.com>
2022-02-21 18:38:36 +0530

    Initially

---
+++ asijole_fiverr_project/PushController.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PushNotification from "react-native-push-notification";
+
+PushNotification.getChannels(function (channel_ids) {
+    console.log(channel_ids);
+});
+
+PushNotification.configure({
+    onRegister: function (token) {
+    },
+
+    onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+
+        PushNotification.localNotification({
+            channelId: notification.channelId,
+            title: notification.title,
+            message: notification.message,
+            allowWhileIdle: false,
+            repeatTime: 1
+        })
+
+    },
+    senderID: "853314511472",
+
+    popInitialNotification: true,
+    requestPermissions: true
+});
+
+
+
+export default class PushController extends Component {
+
+
+    render() {
+        return null;
+    }
+}
